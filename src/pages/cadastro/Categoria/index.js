be27eca374573eb1374cc76bef26f90ebc4242b2
Priscila <priscilaresantos@gmail.com>
2020-07-30 23:55:54 -0300

    Tratamento para consultar a url certa.

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,19 +30,20 @@ function CadastroCategoria() {
     }
 
     useEffect(() => {
-        if (window.location.href.includes('localhost')) {
-            const URL = 'http://localhost:8080/categorias';
-            fetch(URL)
-                .then(async (respostaDoServer) => {
-                    if (respostaDoServer.ok) {
-                        const resposta = await respostaDoServer.json();
-                        setCategorias([...resposta,
-                        ]);
-                        return;
-                    }
-                    throw new Error('Não foi possível pegar os dados');
-                })
-        }
+
+        const URL = window.location.hostname.includes('localhost')
+            ? ''
+            : 'https://aluraflix-pris.herokuapp.com/categorias';
+        fetch(URL)
+            .then(async (respostaDoServer) => {
+                if (respostaDoServer.ok) {
+                    const resposta = await respostaDoServer.json();
+                    setCategorias([...resposta,
+                    ]);
+                    return;
+                }
+                throw new Error('Não foi possível pegar os dados');
+            })
     }, []);
 
 
