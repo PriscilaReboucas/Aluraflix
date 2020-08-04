09145b4ffd156d2f30c02cdb25b2b2da13fd2ce1
Priscila <priscilaresantos@gmail.com>
2020-08-03 23:11:59 -0300

    Cadastro de videos e ajustes na página home

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,38 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import FormField from '../../../components/Carousel/components/FormField';
+import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
+import useForm from '../../../hooks/useForm';
+
+
 
 function CadastroCategoria() {
     const valoresIniciais = {
-        nome: '',
+        titulo: '',
         descricao: '',
         cor: '',
 
     };
+
+    const { handleChange, values, clearForm } = useForm(valoresIniciais);
+
     const [categorias, setCategorias] = useState([]);
-    const [values, setValues] = useState(valoresIniciais);
-
-    // chave: nome, descricao,
-    function setValue(chave, valor) {
-        setValues({
-            ...values,
-            [chave]: valor, // nome:'valor'
-        });
-    }
-
-    function handleChange(infosDoEvento) {
-        setValue(
-            infosDoEvento.target.getAttribute('name'),
-            infosDoEvento.target.value,
-        );
-    }
 
     useEffect(() => {
 
         const URL = window.location.hostname.includes('localhost')
-            ? 'http://localhost/categorias'
+            ? 'http://localhost:8080/categorias'
             : 'https://aluraflix-pris.herokuapp.com/categorias';
         fetch(URL)
             .then(async (respostaDoServer) => {
@@ -60,20 +50,20 @@ function CadastroCategoria() {
                     ...categorias,
                     values,
                 ]);
-                setValues(valoresIniciais);
+                clearForm();
             }}
             >
 
                 <FormField
-                    label="Nome da Categoria:"
+                    label="Titulo da Categoria"
                     type="text"
-                    value={values.nome}
-                    name="nome"
+                    value={values.titulo}
+                    name="titulo"
                     onChange={handleChange}
                 />
 
                 <FormField
-                    label="Descrição:"
+                    label="Descrição"
                     type="textarea"
                     value={values.descricao}
                     name="descricao"
@@ -81,7 +71,7 @@ function CadastroCategoria() {
                 />
 
                 <FormField
-                    label="Cor:"
+                    label="Cor"
                     type="color"
                     value={values.cor}
                     name="cor"
@@ -101,9 +91,9 @@ function CadastroCategoria() {
 
 
             <ul>
-                {categorias.map((categoria, indice) => (
-                    <li key={`${categoria}${indice}`}>
-                        {categoria.nome}
+                {categorias.map((categoria) => (
+                    <li key={`${categoria.titulo}`}>
+                        {categoria.titulo}
                     </li>
                 ))}
             </ul>
